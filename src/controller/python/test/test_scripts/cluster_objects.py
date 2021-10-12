a9e9d65172f0344091666a115b8790b68bac7e2e
Song Guo <songguo@google.com>
2021-10-12 16:47:51 +0800

    Python Cluster Object Codegen

--- src/controller/python/test/test_scripts/cluster_objects.py
+++ src/controller/python/test/test_scripts/cluster_objects.py
@@ -16,7 +16,7 @@
 #
 
 
-from chip.clusters import TestObjects
+import chip.clusters as Clusters
 import logging
 from chip.interaction_model import exceptions as IMExceptions
 
@@ -30,20 +30,20 @@ LIGHTING_ENDPOINT_ID = 1
 class ClusterObjectTests:
     @classmethod
     def TestAPI(cls):
-        if TestObjects.OnOff.id != 6:
+        if Clusters.OnOff.id != 6:
             raise ValueError()
-        if TestObjects.OnOff.Commands.Off.command_id != 0:
+        if Clusters.OnOff.Commands.Off.command_id != 0:
             raise ValueError()
-        if TestObjects.OnOff.Commands.Off.cluster_id != 6:
+        if Clusters.OnOff.Commands.Off.cluster_id != 6:
             raise ValueError()
-        if TestObjects.OnOff.Commands.On.command_id != 1:
+        if Clusters.OnOff.Commands.On.command_id != 1:
             raise ValueError()
-        if TestObjects.OnOff.Commands.On.cluster_id != 6:
+        if Clusters.OnOff.Commands.On.cluster_id != 6:
             raise ValueError()
 
     @classmethod
     async def RoundTripTest(cls):
-        req = TestObjects.OnOff.Commands.On()
+        req = Clusters.OnOff.Commands.On()
         res = await req.send(nodeId=NODE_ID, endpointId=LIGHTING_ENDPOINT_ID)
         if res is not None:
             logger.error(
@@ -52,7 +52,7 @@ class ClusterObjectTests:
 
     @classmethod
     async def RoundTripTestWithBadEndpoint(cls):
-        req = TestObjects.OnOff.Commands.On()
+        req = Clusters.OnOff.Commands.On()
         try:
             await req.send(nodeId=NODE_ID, endpointId=233)
             raise ValueError(f"Failure expected")
@@ -61,11 +61,23 @@ class ClusterObjectTests:
             return
 
     @classmethod
+    async def SendCommandWithResponse(cls):
+        req = Clusters.TestCluster.Commands.TestAddArguments(Arg1=2, Arg2=3)
+        res = await req.send(nodeId=NODE_ID, endpointId=LIGHTING_ENDPOINT_ID, responseType=Clusters.TestCluster.Commands.TestAddArgumentsResponse)
+        if not isinstance(res, Clusters.TestCluster.Commands.TestAddArgumentsResponse):
+            logger.error(f"Unexpected response of type {type(res)} received.")
+            raise ValueError()
+        logger.info(f"Received response: {res}")
+        if res.ReturnValue != 5:
+            raise ValueError()
+
+    @classmethod
     async def RunTest(cls):
         try:
             cls.TestAPI()
             await cls.RoundTripTest()
             await cls.RoundTripTestWithBadEndpoint()
+            await cls.SendCommandWithResponse()
         except Exception as ex:
             logger.error(
                 f"Unexpected error occurred when running tests: {ex}")
